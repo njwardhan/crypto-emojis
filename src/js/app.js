7225206cbceabd1c7a48de249bd7a7c98b280793
njwardhan <njwardhan@gmail.com>
2021-12-12 15:31:22 +0530

    frontend initials

---
+++ src/js/app.js
@@ -0,0 +1,30 @@
+// import Web3 from 'web3'
+
+// App = {
+//     web3provider: null,
+//     contracts: {},
+//     account: '0x0',
+//     loading: false,
+
+//     init: function() {
+//         console.log("App initialized...")
+//         return App.initWeb3();
+//     },
+
+//     initWeb3: function() {
+//         if (typeof web3 !== 'undefined') {
+//           // If a web3 instance is already provided by Meta Mask.
+//           App.web3Provider = window.ethereum;
+//           web3 = new Web3(window.ethereum);
+//         } else {
+//           // Specify default instance if no web3 instance provided
+//           App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
+//           web3 = new Web3(App.web3Provider);
+//         }
+//     },
+
+//     // Main function that will render the entire app
+//     render: function() {
+//         console.log("chal ja bhai pls")
+//     }
+// }
